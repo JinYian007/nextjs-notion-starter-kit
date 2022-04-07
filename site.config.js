925f44600483f102047f0d1d5428592c75dfa010
JinYian007 <103177792+JinYian007@users.noreply.github.com>
2022-04-07 16:59:09 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,23 +1,23 @@
 export default {
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '6694634b3f254c8d802e6b0843ea6d59',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '三生不二的自留地',
+  domain: 'https://www.notion.so/6694634b3f254c8d802e6b0843ea6d59',
+  author: '三生不二',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: 'jinyian007',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
